43afcc3563ea7ccb1aa4ce1181d241c949628c4a
Mike Hudgins <mchudgins@gmail.com>
2016-12-21 15:34:24 -0600

    added the bash scripts from the old cert project to generate the CA certs, keys, etc

---
+++ ca/bin/fromScratch.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# generate the root, intermediate, and fsg CA's in the current working directory
+#
+mkdir root-ca intermediate-ca
+cd root-ca
+../bin/init-root-ca.sh
+../bin/generate-root-CA.sh
+cd -
+
+cd intermediate-ca
+../bin/init-intermediate.sh
+../bin/generate-intermediate.sh
+cd -
+
+#bin/create-bu-ca.sh "Financial Services Group" fsg
